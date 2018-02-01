e93557f8fa0c36f504dc2670da478785f7124e50
copydog <775150@qq.com>
2018-02-01 21:14:33 +0800

    1

--- src/RuaCache.ts
+++ src/RuaCache.ts
@@ -3,25 +3,39 @@ import RuaStorage from 'rua-storage/lib/RuaStorage'
 
 import { RuaCacheInterface } from './Interface'
 import { AbstractRuaPackage } from 'rua-core/lib/Abstractions'
-import AnyData from 'rua-core/lib/Types/AnyData'
-import AnyObject from 'rua-core/lib/Types/AnyObject'
+import { AnyData, AnyObject } from 'rua-core/lib/Types'
+import * as _ from 'lodash'
 
 class RuaCache extends AbstractRuaPackage implements RuaCacheInterface {
 
   protected prefix: string = 'RuaCache-'
 
+  /**
+   * RuaStorage instance
+   *
+   * @type {RuaStorage}
+   */
   protected storage: RuaStorage = storage
 
-  protected count: number
+  /**
+   * Cached data count
+   *
+   * @type {number}
+   */
+  protected count: number = -1
 
+  /**
+   * Cached data keys
+   *
+   * @type {string[]}
+   */
   protected list: string[] = []
 
-  public get(key: string, defaultValue: any): any {
+  public get(key: string, defaultValue: any): AnyData {
     // defaultValue will be returned if no data with the specific key
     if (!this.list.includes(key)) {
       return defaultValue
     }
-
     // retrieve data from cache
     return this.store[key]
   }
@@ -30,16 +44,25 @@ class RuaCache extends AbstractRuaPackage implements RuaCacheInterface {
   }
 
   public remove(key: string): void {
-    delete this.store[key]
+    // Cache removal
+    _.unset(this.store, key)
+    _.pull(this.list, key)
 
+    // Sync list
+    this.storage.set(
+      this.getListKey(),
+      this.list,
+    )
+    // Sync item
+    this.storage.remove(key)
   }
 
   public clear(): void {
     this.count = 0
     this.list = []
-    // data removal
+    // remove all data from storage
     this.storage.remove(this.list)
-    // list removal
+    // remove list from storage
     this.storage.remove(this.getListKey())
   }
 
@@ -64,6 +87,8 @@ class RuaCache extends AbstractRuaPackage implements RuaCacheInterface {
     this.list = <string[]>JSON.parse(list)
     // Load all saved cache data to store
     this.store = <AnyObject>await storage.get(this.list)
+    // Calculate count
+    this.count = this.list.length
   }
 
   protected getListKey(): string {
